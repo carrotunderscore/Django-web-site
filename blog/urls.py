b4e325b6e1a97dfc44ca2d9a3cd40d51ceb782e4
carrotunderscore <robertpergustafsson@gmail.com>
2024-04-29 11:54:23 +0200

    Cleaned up code and removed the mainapp. Now the blog is the main app.

--- blog/urls.py
+++ blog/urls.py
@@ -14,15 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "blog"
 
 urlpatterns = [
+
+    path('authentication/', include('authentication.urls')),
+
     path('', views.blog_view, name='blog_view'),
     path('create_post_view/', views.create_post_view, name='create_post_view'),
     path('publish_post/', views.publish_post, name='publish_post'),
     path('delete_post/', views.delete_post, name='delete_post'),
     path('edit_post/<int:pk>/', views.edit_post_view, name='edit_post'),
+
 ]
